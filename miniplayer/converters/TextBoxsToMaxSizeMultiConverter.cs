0e4ba78f538fbf23938cfd937332935a05c40a92
hoooknew <hoooknew@gmail.com>
2022-12-13 06:29:56 -0500

    There's still a layout issue with the title text. I've added, but not used a converter to try and fix it.

---
+++ miniplayer/converters/TextBoxsToMaxSizeMultiConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace miniplayer.converters
+{
+    internal class TextBoxsToMaxSizeMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.OfType<TextBlock>()
+                .Select(r => MeasureString(r))
+                .Select(r => r.Width)
+                .Max();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Size MeasureString(TextBlock textBlock)
+        {            
+            var formattedText = new FormattedText(
+                textBlock.Text,
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(textBlock.FontFamily, textBlock.FontStyle, textBlock.FontWeight, textBlock.FontStretch),
+                textBlock.FontSize,
+                Brushes.Black,
+                new NumberSubstitution(),
+                1);
+
+            return new Size(formattedText.Width, formattedText.Height);
+        }
+    }
+}
