42c484d4ac9c2cd9983e40537d7e4e9ecff049bf
hoooknew <hoooknew@gmail.com>
2022-12-13 22:16:43 -0500

    I got auth working in a test library

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [Getting an access and refresh token](https://github.com/JohnnyCrazy/SpotifyAPI-NET/blob/master/SpotifyAPI.Web.Examples/Example.TokenSwap/Client/Program.cs)
 [Getting Started with SpotifyAPI-NET](https://johnnycrazy.github.io/SpotifyAPI-NET/docs/getting_started)
 https://johnnycrazy.github.io/SpotifyAPI-NET/docs/pkce
+[PKCE console example](https://github.com/JohnnyCrazy/SpotifyAPI-NET/blob/54f8f8960fbd859781fd971efaca94462ca52468/SpotifyAPI.Web.Examples/Example.CLI.PersistentConfig/Program.cs)
---
+++ test_auth/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+
+namespace test_auth
+{
+    public class Program
+    {
+        private static EmbedIOAuthServer? _server;
+
+        public static async Task Main()
+        {
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile($"appsettings.json");
+            var configuration = builder.Build();
+
+            string? clientId = configuration["clientId"];
+            var (verifier, challenge) = PKCEUtil.GenerateCodes();
+
+            _server = new EmbedIOAuthServer(new Uri("http://localhost:3000/auth/callback"), 3000);
+            await _server.Start();
+
+            //_server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
+            _server.AuthorizationCodeReceived += async (object sender, AuthorizationCodeResponse response) =>
+            {
+                await _server.Stop();
+                var token = await new OAuthClient().RequestToken(
+                    new PKCETokenRequest(clientId!, response.Code, _server.BaseUri, verifier)
+                );
+
+                var spotify = new SpotifyClient(token.AccessToken);
+            };
+            
+
+            var request = new LoginRequest(_server.BaseUri, clientId!, LoginRequest.ResponseType.Code)
+            {
+                CodeChallengeMethod = "S256",
+                CodeChallenge = challenge,
+                Scope = new List<string> 
+                { 
+                    Scopes.AppRemoteControl, 
+                    Scopes.UserReadCurrentlyPlaying, 
+                    Scopes.UserReadPlaybackState, 
+                    Scopes.UserReadPlaybackPosition, 
+                    Scopes.UserModifyPlaybackState 
+                }
+            };
+
+            Uri uri = request.ToUri();
+            try
+            {
+                BrowserUtil.Open(uri);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to open URL, manually open: {0}", uri);
+            }
+
+            Console.ReadKey();
+        }
+
+        //private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response)
+        //{            
+
+        //    var oauth = new OAuthClient();
+
+        //    var tokenRequest = new TokenSwapTokenRequest(new Uri("http://localhost:5001/swap"), response.Code);
+        //    var tokenResponse = await oauth.RequestToken(tokenRequest);
+
+        //    Console.WriteLine($"We got an access token from server: {tokenResponse.AccessToken}");
+
+        //    var refreshRequest = new TokenSwapRefreshRequest(
+        //      new Uri("http://localhost:5001/refresh"),
+        //      tokenResponse.RefreshToken
+        //    );
+        //    var refreshResponse = await oauth.RequestToken(refreshRequest);
+
+        //    Console.WriteLine($"We got a new refreshed access token from server: {refreshResponse.AccessToken}");
+        //}
+    }
+}
