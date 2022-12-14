6869b9383390df68b2dead61b696d080cdf49dea
hoooknew <hoooknew@gmail.com>
2022-12-13 19:27:29 -0500

    changed the window to move on drag

--- miniplayer/MainWindow.xaml.cs
+++ miniplayer/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace miniplayer
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
     }
 }
