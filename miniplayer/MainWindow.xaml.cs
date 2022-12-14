fbb2f439c694915f9e6b95c87d5c5d52d1be0b2d
hoooknew <hoooknew@gmail.com>
2022-12-13 20:34:01 -0500

    added resizing that only works on the left and right sides of the window

--- miniplayer/MainWindow.xaml.cs
+++ miniplayer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,8 @@ namespace miniplayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool ResizeInProcess = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +31,48 @@ namespace miniplayer
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+
+        private void Resize_Init(object sender, MouseButtonEventArgs e)
+        {            
+            if (sender is Rectangle senderRect)
+            {
+                ResizeInProcess = true;
+                senderRect.CaptureMouse();                
+                e.Handled = true;
+            }
+        }
+        private void Resize_End(object sender, MouseButtonEventArgs e)
+        {            
+            if (sender is Rectangle senderRect)
+            {
+                ResizeInProcess = false; ;
+                senderRect.ReleaseMouseCapture();
+                e.Handled = true;                
+            }
+        }
+
+        private void Resizing_Window(object sender, MouseEventArgs e)
+        {            
+            if (ResizeInProcess)
+            {                
+                if (sender is Rectangle senderRect)
+                {
+                    double mouseX = e.GetPosition(this).X;
+                    if (senderRect == this._rightSizeGrip)
+                    {
+                        this.Width = Math.Max(this.MinWidth, mouseX);
+                    }
+                    else if (senderRect == this._leftSizeGrip)
+                    {
+                        this.Width = Math.Max(this.MinWidth, this.Width - mouseX);
+                        this.Left = this.Left + mouseX;
+                    }
+                }
+
+                e.Handled = true;
+            }
         }
     }
 }
