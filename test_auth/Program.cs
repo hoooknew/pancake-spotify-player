aad00223d73ff93fd5d90f1f59cc538bef3d3783
hoooknew <hoooknew@gmail.com>
2022-12-14 17:29:22 -0500

    found a more adaptable version of the auth code

--- test_auth/Program.cs
+++ test_auth/Program.cs
@@ -1,52 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using SpotifyAPI.Web;
 using SpotifyAPI.Web.Auth;
+using Swan.Logging;
+using static SpotifyAPI.Web.Scopes;
 
-namespace test_auth
+namespace Example.CLI.PersistentConfig
 {
-    public class Program
+    //https://github.com/JohnnyCrazy/SpotifyAPI-NET/blob/54f8f8960fbd859781fd971efaca94462ca52468/SpotifyAPI.Web.Examples/Example.CLI.PersistentConfig/Program.cs
+    /// <summary>
+    ///   This is a basic example how to get user access using the Auth package and a CLI Program
+    ///   Your spotify app needs to have http://localhost:3000 as redirect uri whitelisted
+    /// </summary>
+    public class test_auth
     {
-        private static EmbedIOAuthServer? _server;
+        private const string CredentialsPath = "credentials.json";
+        private static readonly string? clientId = Config.ClientId;
+        private static readonly EmbedIOAuthServer _server = new EmbedIOAuthServer(new Uri("http://localhost:3000/auth/callback"), 3000);
 
-        public static async Task Main()
+        private static void Exiting() => Console.CursorVisible = true;
+        public static async Task<int> Main()
         {
-            var builder = new ConfigurationBuilder()
-               .AddJsonFile($"appsettings.json");
-            var configuration = builder.Build();
+            // This is a bug in the SWAN Logging library, need this hack to bring back the cursor
+            AppDomain.CurrentDomain.ProcessExit += (sender, e) => Exiting();
 
-            string? clientId = configuration["clientId"];
+            if (string.IsNullOrEmpty(clientId))
+            {
+                throw new NullReferenceException(
+                  "Please set SPOTIFY_CLIENT_ID via environment variables before starting the program"
+                );
+            }
+
+            if (File.Exists(CredentialsPath))
+            {
+                await Start();
+            }
+            else
+            {
+                await StartAuthentication();
+            }
+
+            Console.ReadKey();
+            return 0;
+        }
+
+        private static async Task Start()
+        {
+            var json = await File.ReadAllTextAsync(CredentialsPath);
+            var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
+
+            var authenticator = new PKCEAuthenticator(clientId!, token!);
+            authenticator.TokenRefreshed += (sender, token) => File.WriteAllText(CredentialsPath, JsonConvert.SerializeObject(token));
+
+            var config = SpotifyClientConfig.CreateDefault()
+              .WithAuthenticator(authenticator);
+
+            var spotify = new SpotifyClient(config);
+
+            var me = await spotify.UserProfile.Current();
+            Console.WriteLine($"Welcome {me.DisplayName} ({me.Id}), you're authenticated!");
+
+            var playlists = await spotify.PaginateAll(await spotify.Playlists.CurrentUsers().ConfigureAwait(false));
+            Console.WriteLine($"Total Playlists in your Account: {playlists.Count}");
+
+            _server.Dispose();
+            Environment.Exit(0);
+        }
+
+        private static async Task StartAuthentication()
+        {
             var (verifier, challenge) = PKCEUtil.GenerateCodes();
 
-            _server = new EmbedIOAuthServer(new Uri("http://localhost:3000/auth/callback"), 3000);
             await _server.Start();
-
-            //_server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
-            _server.AuthorizationCodeReceived += async (object sender, AuthorizationCodeResponse response) =>
+            _server.AuthorizationCodeReceived += async (sender, response) =>
             {
                 await _server.Stop();
-                var token = await new OAuthClient().RequestToken(
-                    new PKCETokenRequest(clientId!, response.Code, _server.BaseUri, verifier)
+                PKCETokenResponse token = await new OAuthClient().RequestToken(
+                  new PKCETokenRequest(clientId!, response.Code, _server.BaseUri, verifier)
                 );
 
-                var spotify = new SpotifyClient(token.AccessToken);
+                await File.WriteAllTextAsync(CredentialsPath, JsonConvert.SerializeObject(token));
+                await Start();
             };
-            
 
             var request = new LoginRequest(_server.BaseUri, clientId!, LoginRequest.ResponseType.Code)
             {
-                CodeChallengeMethod = "S256",
                 CodeChallenge = challenge,
-                Scope = new List<string> 
-                { 
-                    Scopes.AppRemoteControl, 
-                    Scopes.UserReadCurrentlyPlaying, 
-                    Scopes.UserReadPlaybackState, 
-                    Scopes.UserReadPlaybackPosition, 
-                    Scopes.UserModifyPlaybackState 
-                }
+                CodeChallengeMethod = "S256",
+                Scope = new List<string> { UserReadEmail, UserReadPrivate, PlaylistReadPrivate, PlaylistReadCollaborative }
             };
 
             Uri uri = request.ToUri();
@@ -58,27 +103,31 @@ namespace test_auth
             {
                 Console.WriteLine("Unable to open URL, manually open: {0}", uri);
             }
+        }
+    }
 
-            Console.ReadKey();
+    public static class Config
+    {
+        private static IConfiguration? __instance = null;
+        public static IConfiguration Instance
+        {
+            get
+            {
+                if (__instance == null)
+                    lock(typeof(Config))
+                    {
+                        if (__instance == null)
+                        {
+                            var builder = new ConfigurationBuilder()
+                                .AddJsonFile($"appsettings.json", true);
+                            __instance = builder.Build();
+                        }
+                    }
+
+                return __instance;
+            }
         }
 
-        //private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response)
-        //{            
-
-        //    var oauth = new OAuthClient();
-
-        //    var tokenRequest = new TokenSwapTokenRequest(new Uri("http://localhost:5001/swap"), response.Code);
-        //    var tokenResponse = await oauth.RequestToken(tokenRequest);
-
-        //    Console.WriteLine($"We got an access token from server: {tokenResponse.AccessToken}");
-
-        //    var refreshRequest = new TokenSwapRefreshRequest(
-        //      new Uri("http://localhost:5001/refresh"),
-        //      tokenResponse.RefreshToken
-        //    );
-        //    var refreshResponse = await oauth.RequestToken(refreshRequest);
-
-        //    Console.WriteLine($"We got a new refreshed access token from server: {refreshResponse.AccessToken}");
-        //}
+        public static string? ClientId => Instance["clientId"];
     }
 }
