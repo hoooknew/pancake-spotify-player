925a3ba5e37b7e49fc8f3918d141c78bc7cb8c71
hoooknew <hoooknew@gmail.com>
2022-12-19 18:40:50 -0500

    added load and save code to the settings class

--- miniplayer.lib/Settings.cs
+++ miniplayer.lib/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using AutoNotify;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,29 @@ namespace miniplayer.lib
 {
     public partial class Settings
     {
-        private static readonly string _userSettings = Path.Combine(Constants.LOCAL_APP_DATA, @"settings.json");
+        private static readonly string _userSettingsPath = Path.Combine(Constants.LOCAL_APP_DATA, @"settings.json");
+
+        private static Settings? __instance;
+        public static Settings Instance
+        {
+            get
+            {
+                if (__instance == null)
+                    lock(typeof(Settings))
+                        if (__instance == null)
+                        {
+                            if (File.Exists(_userSettingsPath))
+                                __instance = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(_userSettingsPath));
+                            else
+                                __instance = new Settings();
+                        }
+
+                return __instance;
+            }
+        }
 
         public Settings()
-        {            
+        {
         }
 
         [AutoNotify]
@@ -23,5 +43,17 @@ namespace miniplayer.lib
         public double _windowWidth;
         [AutoNotify]
         public double _windowHeight;
+
+        private static void CreateLocalAppFolder()
+        {
+            if (!Directory.Exists(Constants.LOCAL_APP_DATA))
+                Directory.CreateDirectory(Constants.LOCAL_APP_DATA);
+        }
+
+        partial void OnPropertyChanged(string propertyName)
+        {
+            CreateLocalAppFolder();
+            File.WriteAllText(_userSettingsPath, JsonConvert.SerializeObject(this));
+        }
     }
 }
