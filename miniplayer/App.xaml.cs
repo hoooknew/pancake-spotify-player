e60ba4b672bbfb306c0ee51403459ad0196187a5
hoooknew <hoooknew@gmail.com>
2022-12-19 21:34:17 -0500

    moved the resizing and moving to BaseWindow

--- miniplayer/App.xaml.cs
+++ miniplayer/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using miniplayer.ui.controls;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,11 @@ namespace miniplayer
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var w = new MainWindow();
+
+            w.Show();
+        }
     }
 }
--- miniplayer/MainWindow.xaml.cs
+++ miniplayer/MainWindow.xaml.cs
@@ -1,28 +1,14 @@
 ﻿using miniplayer.lib;
 using miniplayer.models;
 using miniplayer.ui;
+using miniplayer.ui.controls;
 using SpotifyAPI.Web;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace miniplayer
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : BaseWindow
     {
-        bool ResizeInProcess = false;
         private readonly PlayerModel _model;
 
         public MainWindow()
@@ -63,53 +49,6 @@ namespace miniplayer
         private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
         {
             this.SaveLocation();
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-                this.DragMove();
-        }
-
-
-        private void Resize_Init(object sender, MouseButtonEventArgs e)
-        {            
-            if (sender is Rectangle senderRect)
-            {
-                ResizeInProcess = true;
-                senderRect.CaptureMouse();                
-                e.Handled = true;
-            }
-        }
-        private void Resize_End(object sender, MouseButtonEventArgs e)
-        {            
-            if (sender is Rectangle senderRect)
-            {
-                ResizeInProcess = false; ;
-                senderRect.ReleaseMouseCapture();
-                e.Handled = true;                
-            }
-        }
-        private void Resizing_Window(object sender, MouseEventArgs e)
-        {            
-            if (ResizeInProcess)
-            {                
-                if (sender is Rectangle senderRect)
-                {
-                    double mouseX = e.GetPosition(this).X;
-                    if (senderRect == this._rightSizeGrip)
-                    {
-                        this.Width = Math.Max(this.MinWidth, mouseX);
-                    }
-                    else if (senderRect == this._leftSizeGrip)
-                    {
-                        this.Width = Math.Max(this.MinWidth, this.Width - mouseX);
-                        this.Left = this.Left + mouseX;
-                    }
-                }
-
-                e.Handled = true;
-            }
         }
 
 
@@ -156,3 +95,5 @@ namespace miniplayer
         }
     }
 }
+//Severity	Code	Description	Project	File	Line	Suppression State
+//Error MC6017	'miniplayer.ui.controls.BaseWindow' cannot be the root of a XAML file because it was defined using XAML. Line 1 Position 18.miniplayer  C:\code\miniplayer\miniplayer\MainWindow.xaml   1
